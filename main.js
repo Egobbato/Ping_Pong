7440039163453fafa817b943c6cb1836eb178d52
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2023-03-07 21:45:45 -0300

    Iniciando Projeto com Canvas

---
+++ main.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+console.log(canvas);
+
+function setup() {
+  canvas.width = ctx = window.innerWidth;
+  canvas.height = ctx = window.innerHeight;
+}
+
+function draw() {}
+
+setup();
