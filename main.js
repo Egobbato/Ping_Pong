5e4ccd2376d4dfa23483bae8def25491e6428daa
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2023-03-08 21:39:39 -0300

    Desenho da Raquetes

--- main.js
+++ main.js
@@ -11,16 +11,25 @@ function setup() {
 setup();
 
 function draw() {
+  //Desenhando o campo//
   canvasEcx.fillStyle = "#286047";
   canvasEcx.fillRect(0, 0, window.innerWidth, window.innerHeight);
-  canvasEcx.fillStyle = "#ffffff";
 
+  /*desenhando a linha central*/
+  canvasEcx.fillStyle = "#ffffff";
   const x = window.innerWidth / 2 - lineWidtd / 2;
   const y = 0;
   const w = lineWidtd;
   const h = window.innerHeight;
-
   canvasEcx.fillRect(x, y, w, h);
+
+  /*Desenhando as raquete esquerda*/
+  canvasEcx.fillStyle = "#ffffff";
+  canvasEcx.fillRect(10, 400, lineWidtd, 200);
+
+  /* Desenhando raquete direita*/
+  canvasEcx.fillStyle = "#ffffff";
+  canvasEcx.fillRect(window.innerWidth - lineWidtd - 10, 200, lineWidtd, 200);
 }
 
 draw();
