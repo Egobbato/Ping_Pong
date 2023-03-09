a96821f33aae66465fa1999ef01b7d59be1d3654
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2023-03-08 21:28:49 -0300

    Desenhando a linha central

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const canvasEl = document.querySelector("canvas"),
   canvasEcx = canvasEl.getContext("2d");
 
+const lineWidtd = 15;
+
 function setup() {
   canvasEl.width = canvasEcx.width = window.innerWidth;
   canvasEl.height = canvasEcx.height = window.innerHeight;
@@ -11,6 +13,14 @@ setup();
 function draw() {
   canvasEcx.fillStyle = "#286047";
   canvasEcx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+  canvasEcx.fillStyle = "#ffffff";
+
+  const x = window.innerWidth / 2 - lineWidtd / 2;
+  const y = 0;
+  const w = lineWidtd;
+  const h = window.innerHeight;
+
+  canvasEcx.fillRect(x, y, w, h);
 }
 
 draw();
